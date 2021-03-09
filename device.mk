942cfa9b4e00acf7e924466c36c1952f44d77de3
Gagan Malvi <malvi@aospa.co>
2021-03-09 23:13:10 +0530

    lime: import keylayouts.
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- device.mk
+++ device.mk
@@ -40,3 +40,11 @@ PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
 
 # Default kernel revision
 TARGET_KERNEL_VERSION := 4.19
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
+    $(LOCAL_PATH)/keylayout/uinput-focal.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-focal.kl \
+    $(LOCAL_PATH)/keylayout/uinput-cdfinger.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-cdfinger.kl
